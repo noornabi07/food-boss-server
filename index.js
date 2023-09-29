b40224b7041140dc2120267b4f50a7b0c2a5d0a3
noornabi07 <noornabiprogram07@gmail.com>
2023-09-29 19:42:55 +0600

    mycart item deleted success

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnuoch3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,12 +42,12 @@ async function run() {
 
 
         // Card related working
-        app.get('/carts', async(req, res) =>{
+        app.get('/carts', async (req, res) => {
             const email = req.query.email;
-            if(!email){
+            if (!email) {
                 res.send([]);
             }
-            const query = {email: email};
+            const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
@@ -57,6 +57,21 @@ async function run() {
             const result = await cartCollection.insertOne(item);
             res.send(result);
         })
+
+        // app.delete("/carts/:id", async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) }
+        //     const result = await cartCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+          })
 
 
 
