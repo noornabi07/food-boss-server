0dd12a9e1ec14580276848ddfec7979d6a554f94
noornabi07 <noornabiprogram07@gmail.com>
2023-09-30 22:28:20 +0600

    users manage and make admin with delete function

--- index.js
+++ index.js
@@ -32,19 +32,38 @@ async function run() {
         const cartCollection = client.db("FoodDb").collection("carts");
 
         // users related apis
-        app.get('/users', async(req, res) =>{
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
 
-        app.post('/users', async(req, res) =>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
-            if(existingUser){
-                return ({message: 'User already existing'});
+            if (existingUser) {
+                return ({ message: 'User already existing' });
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
             res.send(result);
         })
 
@@ -83,7 +102,7 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query);
             res.send(result);
-          })
+        })
 
 
 
