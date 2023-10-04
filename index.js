8a0699e69a51b7a2b6bf4c92c4e6a6b9ff359791
noornabi07 <noornabiprogram07@gmail.com>
2023-10-04 21:27:05 +0600

    added new item from the admin successfullly

--- index.js
+++ index.js
@@ -124,6 +124,12 @@ async function run() {
             res.send(result);
         })
 
+        app.post('/menu', verifyJWT, verifyAdmin, async(req, res) =>{
+            const newItem = req.body;
+            const result = await menuCollection.insertOne(newItem);
+            res.send(result);
+        })
+
         // reviews related apis
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
