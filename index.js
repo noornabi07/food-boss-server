0dd78d8e209e3454b81404c7ece600f37a9b7a4d
noornabi07 <noornabiprogram07@gmail.com>
2023-10-04 23:00:24 +0600

    manage items with delete item success

--- index.js
+++ index.js
@@ -130,6 +130,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: id};
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // reviews related apis
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray();
